18a1e2d6a6fb6d0235c21e28e95ee403191266b9
Ashim Ghosh <ashim.atiit@gmail.com>
2025-01-07 09:37:41 +0530

    resources/sync-proj-todos.sh: add few clinic todos.org

--- .dotfiles/resources/sync-proj-todos.sh
+++ .dotfiles/resources/sync-proj-todos.sh
@@ -6,12 +6,13 @@ DEST_PATH=~/private/orgzly/
 /bin/cp ~/ashim/projbg/kaizen/docs/todos.org $DEST_PATH/kaizen-todos.org
 /bin/cp ~/ashim/projbg/fitness/docs/todos.org $DEST_PATH/fitness-todos.org
 
+/bin/cp /Users/ashimghosh/clinic/bunny-whites/clinical/todos.org $DEST_PATH/clinic-clinical-todos.org
 /bin/cp ~/clinic/bunny-whites/admin/todos.org $DEST_PATH/clinic-admin-todos.org
+/bin/cp ~/clinic/bunny-whites/finance/todos.org $DEST_PATH/clinic-finance-todos.org
 /bin/cp ~/clinic/bunny-whites/bizdev/todos.org $DEST_PATH/clinic-bizdev-todos.org
 /bin/cp ~/clinic/bunny-whites/bizdev/new-clinic-nigdi/todos.org $DEST_PATH/new-clinic-nigdi-todos.org
+/bin/cp ~/clinic/bunny-whites/bizdev/new-clinic-mahalunge/todos.org $DEST_PATH/new-clinic-mahalunge-todos.org
 /bin/cp ~/clinic/bunny-whites/brand/marketing/todos.org $DEST_PATH/clinic-marketing-todos.org
 
 /bin/cp ~/family/projfg/baby/docs/todos.org $DEST_PATH/family-baby-todos.org
-
-/bin/cp ~/ashim/projbg/kaizen/org/kitchen.org $DEST_PATH/0-kitchen.org
 
