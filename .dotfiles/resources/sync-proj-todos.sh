ebce07ab0007c1a346dbca63f82549e6b5ecfc15
Ashim Ghosh <ashim.atiit@gmail.com>
2026-01-17 12:23:22 +0530

    fix(sync-proj-todos.sh): remove tilde expansion from env var

--- .dotfiles/resources/sync-proj-todos.sh
+++ .dotfiles/resources/sync-proj-todos.sh
@@ -6,7 +6,7 @@ DEST_PATH=~/private/orgzly/
 /bin/cp ~/ashim/projbg/kaizen/docs/todos.org $DEST_PATH/kaizen-todos.org
 /bin/cp ~/ashim/projbg/fitness/docs/todos.org $DEST_PATH/fitness-todos.org
 
-/bin/cp /Users/ashimghosh/clinic/bunny-whites/clinical/todos.org $DEST_PATH/clinic-clinical-todos.org
+/bin/cp ~/clinic/bunny-whites/clinical/todos.org $DEST_PATH/clinic-clinical-todos.org
 /bin/cp ~/clinic/bunny-whites/admin/todos.org $DEST_PATH/clinic-admin-todos.org
 /bin/cp ~/clinic/bunny-whites/finance/todos.org $DEST_PATH/clinic-finance-todos.org
 /bin/cp ~/clinic/bunny-whites/bizdev/todos.org $DEST_PATH/clinic-bizdev-todos.org
